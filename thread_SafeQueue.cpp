6fcdeb45a92f3a835ad786bc1eb698a9dc063c64
Yury <32190920+Link-God@users.noreply.github.com>
2019-01-17 17:58:17 +0300

    Update thread_SafeQueue.cpp

--- thread_SafeQueue.cpp
+++ thread_SafeQueue.cpp
@@ -2,38 +2,61 @@
 #include <queue>
 #include <thread>
 
-template <typename T>
-class SafeQueue
+template <typename t>
+class threadsafe_queue
 {
-    std::mutex Mutex;
-    std::queue<T> Queue;
-
+private:
+	mutable std::mutex mut;
+	std::queue<std::shared_ptr<t> > data_queue;
+	std::condition_variable data_cond;
 public:
-    SafeQueue()
-    {
-    }
-    T Pop()
-    {
-        std::lock_guard<std::mutex> lg(Mutex);
-        if(Queue.empty()) throw std::logic_error "empty_Queue";
-        T temp = Queue.front();
-        Queue.pop();
-        return temp;
-    }
-    void Push(const T& Value)
-    {
-        std::lock_guard<std::mutex> lg(Mutex);
-        Queue.push(Value);
-    }
-    bool TryPop()
-    {
-        std::defer_lock_t t;
-        std::unique_lock<std::mutex> lg(Mutex, t);
-        if (lg.try_lock() && !Queue.empty())
-        {
-            Queue.pop();
-            return true;
-        }
-        return false;
-    }
+	threadsafe_queue() {}
+	
+	void wait_and_pop(t& value)
+	{
+		std::unique_lock<std::mutex> lk(mut);
+		data_cond.wait(lk, [this] {return !data_queue.empty(); });
+		value = std::move(*data_queue.front());
+		data_queue.pop();
+	}
+	bool try_pop(T& value)
+	{
+		std::lock_guard<std::mutex> lk(mut);
+		if (data_queue.empty())
+			return false;
+		value = std::move(*data_queue.front());
+		data_queue.pop();
+		return true;
+	}
+
+	std::shared_ptr<t> wait_and_pop()
+	{
+		std::unique_lock<std::mutex> lk(mut);
+		data_cond.wait(lk, [this] {return !data_queue.empty(); });
+		std::shared_ptr<t> res = data_queue.front();
+		data_queue.pop();
+		return res;
+	}
+	std::shared_ptr<t> try_pop()
+	{
+		std::lock_guard<std::mutex> lk(mut);
+		if(data_queue.empty())
+			return std::shared_ptr<t>();
+		std::shared_ptr<t> res = data_queue.front();
+		data_queue.pop();
+		return res;
+	}
+	void push(t new_value)
+	{
+		std::shared_ptr<t> data(
+			std::make_shared<t>(std::move(new_value)));
+		std::lock_guard<std::mutex> lk(mut);
+		data_queue.push(data);
+		data_cond.notify_one();
+	}
+	bool empty() const
+	{
+		std::lock_guard<std::mutex> lk(mut);
+		return data_queue.empty();
+	}
 };
